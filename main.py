a669594f815b1a8c67a4af083e12842689679d50
vinicius-alves <vinicius.alves@poli.ufrj.br>
2018-04-07 19:26:49 -0300

    Decomposicao LU funcionando

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import my_lib  
 
 matriz_linhas = [
-	[1,0,0],
-	[0,1,0],
-	[0,0,1]
+	[5,1,0],
+	[3,2,7],
+	[2,0,1]
 ]
 
 matriz = my_lib.criar_matriz(matriz_linhas)
 
-print(my_lib.transposta(matriz))
+matriz_L, matriz_U = my_lib.decomposicaoLU(matriz)
--- my_lib/lib.py
+++ my_lib/lib.py
@@ -9,6 +9,12 @@ def determinante(matriz):
 	
 def e_inversivel(matriz):
 	return np.linalg.cond(matriz) < 1/sys.float_info.epsilon
+
+def e_quadrada(matriz):
+	return 	matriz.shape[0] == matriz.shape[1]
+
+def e_nao_singular(matriz):
+	return e_inversivel(matriz)
 	
 def inversa(matriz):
 	return np.linalg.inv(matriz)
@@ -18,3 +24,36 @@ def e_simetrica(matriz):
 	
 def transposta(matriz):
 	return np.transpose(matriz)
+
+def decomposicaoLU(matriz):
+
+	if(not(e_quadrada(matriz))):
+		raise ArithmeticError("DecomposiçãoLU não aplicável, a matriz não é quadrada")
+
+	if(not(e_nao_singular(matriz))):
+		raise ArithmeticError("DecomposiçãoLU não aplicável, a matriz é singular")
+
+	size = matriz.shape[0]
+	matriz_L = np.zeros(matriz.shape)
+	matriz_U = np.zeros(matriz.shape)
+
+	for i in range(0,size):
+
+		for k in range(i,size):
+
+			soma = 0;
+			for j in range(0,i):
+				soma += (matriz_L[i][j] * matriz_U[j][k])
+			matriz_U[i][k] = matriz.A[i][k] - soma
+
+		for k in range(i,size):
+			if(i ==k):
+				matriz_L[i][k] =1
+			else:
+				soma = 0;
+				for j in range(0,i):
+					soma += (matriz_L[k][j] * matriz_U[j][i])
+				matriz_L[k][i] = (matriz.A[k][i] - soma)/matriz_U[i][i]
+
+
+	return matriz_L, matriz_U
