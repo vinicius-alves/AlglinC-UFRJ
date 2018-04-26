4c41350158c1d6e2fde8dc2ca9a3ab5de9b9ba8f
vinicius-alves <vinicius.alves@poli.ufrj.br>
2018-04-25 21:46:41 -0300

    Questão regressão polinomial da lista 3 implementada

--- my_lib/regressao_linear.py
+++ my_lib/regressao_linear.py
@@ -38,7 +38,7 @@ def regredir_fx_b1x_b2(array_X, array_Y, exibir_grafico = False):
 
 	if(exibir_grafico):
 
-		X = np.arange(array_Y[0]-50, array_Y[-1]+50, 0.1)
+		X = np.arange(array_X[0]-50, array_X[-1]+50, 0.1)
 		Y = np.zeros(len(X))
 
 		for i in range(len(X)):
@@ -53,6 +53,49 @@ def regredir_fx_b1x_b2(array_X, array_Y, exibir_grafico = False):
 		plt.show()
 
 	return b1, b2
+
+#Exercício 5
+
+def regredir_para_polinomio_grau_2(array_X, array_Y, exibir_grafico = False):
+
+	matriz_Y = transposta(criar_matriz(array_Y))
+	matriz_P = transposta(criar_matriz([np.ones(len(array_X)),np.power(array_X,1),np.power(array_X,2)]))
+
+	matriz_P_transposta = transposta(matriz_P)
+
+	matriz_B = inversa(matriz_P_transposta*matriz_P)*(matriz_P_transposta*matriz_Y)
+
+	b0 = matriz_B.A[0][0]
+	b1 = matriz_B.A[1][0]
+	b2 = matriz_B.A[2][0]
+
+	if(exibir_grafico):
+
+		X = np.arange(array_X[0]-5, array_X[-1]+5, 0.1)
+		Y = np.zeros(len(X))
+
+		for i in range(len(X)):
+			Y[i] = b0 + b1*X[i] + b2*np.power(X[i],2)
+
+		plt.plot(X,Y,"")
+		plt.plot(array_X,array_Y,"ro")
+
+		b2_str = str("%.1f" % b2)
+		b1_str = "+"+str("%.1f" %b1) if b1 > 0 else str("%.1f" %b1)
+		b0_str = "+"+str("%.1f" %b0) if b0 > 0 else str("%.1f" %b0)
+	
+		
+		plt.title(u"Exercício 4 - regressão f(x) = " + b2_str+"*X^2 "+ b1_str+"X^1 " + b0_str)
+		plt.ylabel("Y")
+		plt.xlabel("X")
+		plt.show()
+
+
+	return matriz_B.T.A[0]
+
+
+
+
 
 	
 
--- terceiro_trabalho.py
+++ terceiro_trabalho.py
@@ -31,7 +31,7 @@ funcao_exercicio_2 = array_B
 
 print"Exercício 3"
 
-b1,b2 = my_lib.regredir_fx_b1x_b2(array_X,array_Y, exibir_grafico=True) 
+b1,b2 = my_lib.regredir_fx_b1x_b2(array_X,array_Y, exibir_grafico=False) 
 
 def valor_regressao_exercicio3_no_ponto(ponto):
 	return b1*(ponto**b2)
@@ -45,6 +45,12 @@ my_lib.print_polinomio(polinomioLagrange)
 
 funcao_exercicio_4 = polinomioLagrange
 
+print "Exercício 5"
+
+polinomioGrau2 = my_lib.regredir_para_polinomio_grau_2(array_X, array_Y, exibir_grafico = True)
+
+funcao_exercicio_5 = polinomioGrau2
+
 print "Exercício 6"
 
 ponto_alvo = 3.5
@@ -55,5 +61,5 @@ print "   primeiro exercício: " +str("%.3f" % my_lib.valor_polinomio_no_ponto(f
 print "   segundo  exercício: " +str("%.3f" % my_lib.valor_polinomio_no_ponto(funcao_exercicio_2,ponto_alvo))
 print "   terceiro exercício: " +str("%.3f" % valor_regressao_exercicio3_no_ponto(ponto_alvo))
 print "   quarto   exercício: " +str("%.3f" % my_lib.valor_polinomio_no_ponto(funcao_exercicio_4,ponto_alvo))
-#print "Valor do ponto " + str(ponto_alvo) + " na função do quinto   exercício: " +str(my_lib.valor_polinomio_no_ponto(funcao_exercicio_5,ponto_alvo))
+print "   quinto   exercício: " +str("%.3f" % my_lib.valor_polinomio_no_ponto(funcao_exercicio_5,ponto_alvo))
 
