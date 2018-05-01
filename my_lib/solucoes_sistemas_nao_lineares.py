0e67ada7e09991af0cbfee87e2428b4f1bc0eba7
Vinicius Almeida Alves <vinicius.alves@poli.ufrj.br>
2018-05-01 16:00:32 -0300

    Método da bisseção generalizado e aplicado, desenvolvido método para visualizar funções

--- my_lib/solucoes_sistemas_nao_lineares.py
+++ my_lib/solucoes_sistemas_nao_lineares.py
@@ -2,26 +2,57 @@
 
 from .metodos_basicos import *
 import numpy as np
+import matplotlib.pyplot as plt
+from inspect import getsource
+
+def visualizar_funcao(function, xmin, xmax, xzero = []):
+	array_x = np.arange(xmin,xmax,0.1)
+	array_y = function(array_x)
+
+	plt.plot(array_x,array_y)
+
+	if(len(xzero)>0):
+		yzero = function(xzero)
+		plt.plot(xzero,yzero,"ro")
+
+	plt.title(getsource(function))
+	plt.ylabel("f(x)")
+	plt.xlabel("x")
+	plt.show()
 
 def bissecao(function, a = -10, b = 10, search_a_b = False ,tol = 0.01):
 
 	if(search_a_b):
 		while( np.sign(function(a)) + np.sign(function(b)) != 0 ):
-			a = np.random.randint(-100,100)
-			b = np.random.randint(-100,100)
+			a = np.random.randint(-1000,1000)
+			b = np.random.randint(-1000,1000)
 
 	else:
 		if(np.sign(function(a)) + np.sign(function(b)) != 0):
 			raise ArithmeticError("Bisseção não possível: f("+str(a)+") e f("+str(b)+") não possuem sinais opostos")
-			
+	
+	if(function(a) < function(b)):
+		inclinacao_positiva = True
+	else:
+		inclinacao_positiva = False
 
 	while(np.abs(b-a)>tol):
 		xi = (a+b)/2.0
 		fi = function(xi)
 
-		if (fi>0):
-			b = xi
+		if(inclinacao_positiva):
+
+			if (fi>0):
+				b = xi
+			else:
+				a = xi
+
 		else:
-			a = xi
+
+			if (fi<0):
+				b = xi
+			else:
+				a = xi
 
 	return xi
+
--- quarto_trabalho.py
+++ quarto_trabalho.py
@@ -1,12 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import my_lib  
+import my_lib
+import numpy as np
+from inspect import getsource
 
-function = lambda x : x + 2
-
-#x_zero = my_lib.bissecao(function = function, search_a_b = True)
-x_zero = my_lib.bissecao(function = function, a = -10 , b = 10 )
-
-print "\nx_zero = "+str(x_zero)+"\n"
+print '\nMétodo da Bisseção:\n' 
 
 # Aplicação 1
+
+g = 9.806
+k = 0.00341
+
+function_1 = lambda x : np.log(np.cosh(x*np.sqrt(g*k))) - 50
+
+print getsource(function_1)
+
+xzero = np.zeros(2)
+
+xzero[0] = my_lib.bissecao(function = function_1, a = -300 , b = 0 )
+xzero[1] = my_lib.bissecao(function = function_1, a = 0  , b = 300 )
+
+for i in range(len(xzero)):
+	print "xzero["+str(i)+"] = "+str(xzero[i])
+
+#my_lib.visualizar_funcao(function = function_1, xmin = -600, xmax = 600, xzero = xzero)
+
+# Aplicação 2
+
+function_2 = lambda x : 4*np.cos(x) - np.exp(2*x)
+
+print getsource(function_2)
+
+xzero = np.zeros(1)
+
+xzero[0] = my_lib.bissecao(function = function_2, a = -20  , b = 4 )
+
+for i in range(len(xzero)):
+	print "xzero["+str(i)+"] = "+str(xzero[i])
+
+#my_lib.visualizar_funcao(function = function_2, xmin = -20, xmax = 4, xzero = xzero)
